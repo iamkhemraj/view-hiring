4ef8d2ebbd10a75638eafab2fe0a5a65878817e1
Khemraj <khemraj@rwit.io>
2024-06-10 14:21:40 +0530

    Get user role in session

--- profile.php
+++ profile.php
@@ -8,13 +8,17 @@ if (!isset($_SESSION['access_token'])) {
 $token = $_SESSION['access_token'];
 $options = [
     'http' => [
-        'header'  => "Authorization: Bearer $token\r\n",
-        'method'  => 'GET',
+        'header' => "Authorization: Bearer $token\r\n",
+        'method' => 'GET',
     ],
 ];
-$context  = stream_context_create($options);
-$result = file_get_contents('http://localhost:5000/api/profile', false, $context);
-$user = json_decode($result, true);
+$context   = stream_context_create($options);
+$result    = file_get_contents('http://localhost:5000/api/profile', false, $context);
+$user      = json_decode($result, true);
+
+// Store the user's role in the session
+$_SESSION['role'] = $user['role'];
+
 ?>
 
 <!DOCTYPE html>
@@ -22,43 +26,71 @@ $user = json_decode($result, true);
 <head>
     <title>User Profile</title>
     <!-- Latest compiled and minified CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <style>
-    label{
-    display: block;
-    width:100%;
+    label {
+        display: block;
+        width: 100%;
+    }
+    .logout {
+        text-align: right;
+        margin: 10px;
+    }
+    .profile {
+        border: 0px solid;
+        width: 102px;
+        border-radius: 100px;
+        overflow: hidden;
+    }
+    img#myImg {
+        width: 100px;
+        margin-bottom: 12px;
     }
     </style>
 </head>
 <body>
     <div class="container mt-5">
-		<div class="row justify-content-center">
-			<div class="col-md-5">
-				<div class="card">
-					<div class="card-header h4 m-0 text-center"> User Profile </div>
-					<div class="card-body">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header h4 m-0 text-center"> <?= ucfirst($_SESSION['role']) ?> </div>
+                    <div class="card-body">
+                        <div class="logout">
+                            <a href="logout.php" class="btn btn-warning">Logout</a>
+                        </div>
+                        <div class="profile">
+                             <?php 
+                                $profile = isset($user['profile']) ? $user['profile'] : '' ;
+                              ?>
+                           <img id="myImg" src="uploads/<?= !empty($profile) ? $profile : 'images.png' ?>" alt="<?= $profile ?>">
+                        </div>
                         <table class="table table-striped">
                             <tr>
-                                <th>Id: </th> <td> <?= $user['id'] ?> </td>
+                                <th>Dashboard</th> 
+                                <th colspan="7">Profile</th>
                             </tr>
                             <tr>
-                                <th>Name: </th> <td> <?= $user['name'] ?> </td>
+                                <th rowspan="7">
+                                    <a href="SuperAdmin/adminRegister.php" class="text-warning d-block">Register admin</a>
+                                    <a href="SuperAdmin/userRegister.php" class="text-warning d-block">Register user</a>
+                                    <a href="SuperAdmin/userManage.php" class="text-warning d-block">Manage user</a>
+                                </th>
+                               
                             </tr>
                             <tr>
-                                <th>Email </th> <td> <?= $user['email'] ?> </td>
+                                <td>Name: <?= $user['name'] ?></td>
                             </tr>
                             <tr>
-                                <th>Name </th> <td> <?= $user['name'] ?> </td>
+                                <td>E-mail: <?= $user['email'] ?></td>
                             </tr>
                         </table>
-                        <a href="logout.php" class="btn btn-primary">Logout</a>
                     </div>
                 </div>
-			</div>
-		</div>
-	</div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
