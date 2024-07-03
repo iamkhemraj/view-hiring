ecc579b8188d12ddba2679bcbf6f7f6a311fe8a1
Khemraj <khemraj@rwit.io>
2024-07-03 10:01:42 +0530

    Added J-query  for  multi delete user

--- SuperAdmin/usersremove.php
+++ SuperAdmin/usersremove.php
@@ -40,11 +40,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'ignore_errors' => true, // Capture response even if it fails
         ],
     ];
-
+    // print_r($options);
     $context = stream_context_create($options);
     $result = file_get_contents(BASE_URL . '/super_admin/delete', false, $context);
     $resultData = json_decode(  $result , true);
-   
+
     if ($result !== false) {
         $response = json_decode($result, true);
         if (isset($response['status'])) {
@@ -74,34 +74,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
                 <div class="card-header h4 m-0 text-center">Users Remove</div>
                 <div class="card-body">
-                    <table class="table table-striped text-left">
+                <form id="multiuserdeleteForm" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <table class="table table-striped text-right">
                         <tr>
+                            <th>Multi/Delete</th>
                             <th>Sr No.</th>
                             <th>User Name</th>
                             <th>User Role</th>
-                            <th>Delete</th>
                         </tr>
                         <?php if (!empty($allAssignedUsers)): ?>
                             <?php foreach ($allAssignedUsers as $key => $user): ?>
                                 <tr>
+                                    <td><input type="checkbox" name="userId[]" value = "<?= $user['id'] ?>"></td>
                                     <td><?= $key + 1 ?></td>
                                     <td><?= $user['name']; ?></td>
                                     <td><?= $user['role']; ?></td>
-                                    <td>
-                                    <form id="deleteForm_<?= $user['id'] ?>" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                                        <input type="hidden" name="userId" value="<?= $user['id'] ?>">
-                                        <button type="button" class="btn btn-danger btn-sm" onclick="confirmDelete(<?= $user['id'] ?>)">Delete</button>
-                                    </form>
-                                    </td>
                                 </tr>
                             <?php endforeach; ?>
+                            <tr>
+                                <td colspan="5" style = margin-right:34px;>
+                                    <input type="checkbox" name="usersId" id="check_all" > Check all 
+                                    <button type="button" class="btn btn-danger btn-sm" onclick="confirmAlldelete()">Delete</button>
+                                </td>
+                            </tr>
                         <?php else: ?>
                             <tr>
                                 <td colspan="3">No users found</td>
                             </tr>
                         <?php endif; ?>
                     </table>
-                    <div class="text-center" >
+                      </form>
+                        <div class="text-center" >
                             <a class="hover-link " style="cursor: pointer;" onclick="navigate()">Go to back</a>
                         </div>
 
@@ -116,12 +119,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 </div>
 <script>
-    function confirmDelete(userId) {
+    function confirmAlldelete() {
         if (confirm("Are you sure you want to delete this user?")) {
             // If user confirms deletion, submit the form
-            document.getElementById('deleteForm_' + userId).submit();
+            document.getElementById('multiuserdeleteForm').submit();
+
         }
     }
+    $("#check_all").click(function(){
+        $('input:checkbox').not(this).prop('checked', this.checked);
+    });
 </script>
 <?php include('../footer.php');?>
 
